529c7afe62fe025bb9d27fa8c784da08a50ed13e
m26imta <145836885+m26imta@users.noreply.github.com>
2023-09-23 18:16:41 +0700

    them mot vai thay doi trong options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -48,3 +48,34 @@ vim.opt.listchars = {
   -- eol = '↲',
   eol = '↴',
 }
+
+-- set showbreak=↪\
+-- vim.opt.listchars:append "eol:↴"
+--[[vim.cmd [[
+set listchars=tab:→\ ,eol:↲,nbsp:␣,trail:•,extends:⟩,precedes:⟨
+]]
+vim.opt.cmdheight = 1
+vim.opt.completeopt = { "menu", "menuone", "noselect" } -- mostly just for cmp
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.termencoding = "utf-8"
+vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+vim.opt.backup = false  -- no annoying backup file
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+vim.opt.undofile = true -- enable persitent undo
+-- abcdefghijklmnopqrstuvwxyz
+-- ABCDEFGHIJKLMNOPQRSTUVWXYZ
+-- oO08 iIlL1 g9qCGQ ~-+= => <= <>(){}[]
+-- vim.opt.guifont="agave Nerd Font Mono:h12"
+-- if vim.fn.exists('g:neovide')==1 then print("neovide detected") end
+-- vim.cmd[[set guifont=CodeNewRoman\ NFM,monospace:h14:b]]
+vim.opt.iskeyword:append("-") -- "-" is also a single word. So dw can delete "a-whole-long-word"
+--[[
+vim.opt.shortmess:append "c"
+vim.opt.whichwrap:append("<,>,[,],h,l")  -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.formatoptions:remove({ "c", "r", "o" })  -- This is a sequence of letters which describes how automatic formatting is to be done
+]]
+
+vim.opt.timeoutlen = 500
+-- vim.opt.updatetime = 300
