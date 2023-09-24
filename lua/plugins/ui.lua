35928e2e395702c0dc7a3b3e177408bc064aadcc
nv2 <tkksaa>
2023-09-24 23:41:03 +0700

    add lualine
    add dependencies
    
    load core module
    
    add noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -16,4 +16,38 @@ return {
       require("plugins.configs.bufferline")
     end,
   },
+  --
+  -- lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    --event = "BufEnter", lazy = true,
+    event = "VeryLazy",
+    config = function()
+      require("plugins.configs.lualine")
+    end,
+  },
+  --
+  -- noice
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function ()
+      require("plugins.configs.noice")
+    end
+  },
+  --
+  -- dependencies
+  {
+    {"nvim-lua/plenary.nvim", lazy = true},
+    {"nvim-tree/nvim-web-devicons", lazy = true},
+    {"MunifTanjim/nui.nvim", lazy = true},
+  }
 }
