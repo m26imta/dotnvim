e52744215f8522cb1baf7cd2be821bc6b35a94c9
nv2 <tkksaa>
2023-09-29 01:41:21 +0700

    edit cmp.lua : mapping2 - disable Enter & using Tab for completion

--- lua/plugins/configs/ide/cmp.lua
+++ lua/plugins/configs/ide/cmp.lua
@@ -113,12 +113,12 @@ local nvchadmapping = {
 }
 
 
-local mymapping = {
+local mapping = {
   -- ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
   -- ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
   -- ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
   -- ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-  ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+  ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
   ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
   ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
   ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
@@ -153,7 +153,44 @@ local mymapping = {
   end, { "i", "s", }),
 }
 
-local myformatting = {
+local mapping2 = {
+  ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+  ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+  ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+  ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+  ["<C-e>"] = cmp.mapping {
+    i = cmp.mapping.abort(),
+    c = cmp.mapping.close(),
+  },
+  -- Accept currently selected item. If none selected, `select` first item.
+  -- Set `select` to `false` to only confirm explicitly selected items.
+  ["<CR>"] = cmp.mapping.confirm { select = false },
+  ["<Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif luasnip.expandable() then
+      luasnip.expand()
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    elseif check_backspace() then
+      fallback()
+    else
+      fallback()
+    end
+  end, { "i", "s", }),
+  ["<S-Tab>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, { "i", "s", }),
+}
+
+
+local formatting = {
   fields = { 'abbr', 'kind', 'menu', },
   -- https://github.com/onsails/lspkind.nvim#option-2-nvim-cmp
   format = lspkind.cmp_format {
@@ -219,8 +256,8 @@ local options = {
       },
     },
   }),
-  mapping = mymapping,
-  formatting = myformatting,
+  mapping = mapping2,
+  formatting = formatting,
 }
 
 
